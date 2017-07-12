d86749f2e507260e377d5ca4d01a0ddad123081f
alan <leggitta3@gmail.com>
2017-07-12 13:23:43 -0700

    returns number of hits per database for search term

--- search.py
+++ search.py
@@ -1,39 +1,12 @@
 from Bio import Entrez
+import pandas as pd
+
 Entrez.email = "leggitta3@gmail.com"
 
-term = "ppg+blood+pressure"
-db = "pubmed"
+term = "ppg blood pressure"
 
 query_handle = Entrez.egquery(term=term)
-record = Entrez.read(query_handle)
-print(record)
-
-search_handle = Entrez.esearch(db=db, term=term)
-records = Entrez.read(search_handle)
-id_list = records['IdList']
-
-for i in id_list:
-    # h = Entrez.e 
-    print(i)
-
-'''
-pmid = 21135640
-
-# get list of ids that cite this article
-h = Entrez.elink(
-    dbfrom="pubmed", linkname="pubmed_pubmed_citedin", id=pmid)
-r = Entrez.read(h)
-h.close()
-
-cited_by = [int(l['Id']) for l in r[0]['LinkSetDb'][0]['Link']]
-print(cited_by)
-
-# get list of ids that this article cites
-h = Entrez.elink(
-    dbfrom="pubmed", linkname="pubmed_pubmed_refs", id=24752654)
-r = Entrez.read(h)
-h.close()
-
-cites = [int(l['Id']) for l in r[0]['LinkSetDb'][0]['Link']]
-print(cites)
-'''
+query_record = Entrez.read(query_handle)
+results = query_record['eGQueryResult']
+query_table = pd.DataFrame(results)
+print(query_table)
